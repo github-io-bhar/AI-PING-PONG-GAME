373a6502cde3dbb6c323088c81807b9eef192ac2
github-io-bhar <86400328+github-io-bhar@users.noreply.github.com>
2023-03-03 15:43:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*created by prashant shukla */
+/*created by abhyudaya swarnim bhargav*/
 var paddle2 =10,paddle1=10;
 var paddle1X = 10,paddle1Height = 110;
 var paddle2Y = 685,paddle2Height = 70;
@@ -18,9 +18,17 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+function modelLoaded(){
+  console.log('PoseNet Is Initialized');
 }
 function draw(){
   background(0); 
+  image(video, 0, 0, 700, 600);
   fill("black");
   stroke("black");
   rect(680,0,20,700);
